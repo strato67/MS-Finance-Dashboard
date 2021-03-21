639ffbc5b4447059b7951cce5fa061cd794c7597
Saaransh Sharma <43897976+strato67@users.noreply.github.com>
2021-03-20 23:25:41 -0400

    news api setup

--- server.py
+++ server.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request
 from search import Search
 import yfinance as yf 
 from newsapi import NewsApiClient
+import requests
 
 # Initialize news api and test documentation
 newsapi = NewsApiClient(api_key='2472f965378e4f079bc9f24dac794181')
-#top_headlines = newsapi.get_top_headlines(q='bitcoin')
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pass'
@@ -25,9 +26,7 @@ def mainPage():
             result=result[0]
             stock = yf.Ticker(result)
             info = list(stock.info.items())
-            financials = stock.financials.items()
-
-            
+                        
             return render_template('stockboard.html',info=info,result=result, length = len(info))
         except:
             return render_template('stockboard.html')
@@ -35,6 +34,13 @@ def mainPage():
 
 if __name__ =='__main__':
     app.run()
-    msft = yf.Ticker("MSFT")
-    print(msft.financials.keys)
+    """url = ('https://newsapi.org/v2/everything?'
+           'q=Apple&'
+           'from=2021-03-21&'
+           'sortBy=popularity&'
+           'apiKey=2472f965378e4f079bc9f24dac794181')
+    response = requests.get(url)
+    print (response.json())"""
+    
+    
                                     
