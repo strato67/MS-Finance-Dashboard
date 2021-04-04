574d4253de3cb4a324f5fb0ea645183c4f09460d
Saaransh Sharma <43897976+strato67@users.noreply.github.com>
2021-04-04 00:22:49 -0400

    Revert "Revert "m""
    This reverts commit e226dc8deb687aa670aa668696bd5d4a84745c5b.

--- search.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-#Creating search bar variables
-class Search(FlaskForm):
-    tickerSearch = StringField('Ticker')
-    submit = SubmitField('Search')
